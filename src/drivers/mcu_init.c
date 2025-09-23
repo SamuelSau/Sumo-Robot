d5bc5c2a14bc6c3f468c7988e7cd24b45499c0a7
Sammy <samdrinkswater@gmail.com>
2025-09-23 10:01:09 -0400

    feat(init): increase clock speed from 1 MHz to 16 MHz
    There is no particular reason on why increasing the clock speed is necessary.
    Power consumption and performance will not be a limiting factor to this change.

--- src/drivers/mcu_init.c
+++ src/drivers/mcu_init.c
@@ -1,16 +1,41 @@
 #include "drivers/mcu_init.h"
 #include "drivers/io.h"
+#include "common/assert_handler.h"
 #include <msp430.h>
 
 /* Watchdog is enabled by default and resets microcontroller repeatedly it not
  * explicitly stopped
  * */
 
+/* TI calibrates each unit during manufacturing and stores calibration value in memory to
+ * achieve similar clock rate between different units. This is a sanity check if calibration
+ * data still exists.
+ * */
+
+static void init_clocks()
+{
+	ASSERT(CALBC1_1MHZ != 0xFF && CALBC1_16MHZ != 0xFF);
+	/* Configures internal oscillator (main clock) to run 16MHz.
+	   Clock will be used a reference for DCO.
+	*/
+	BCSCTL1 |= CALBC1_16MHZ;
+	// Sets 16MHz for digitally controller oscillator (DCO)
+	DCOCTL |= CALDCO_16MHZ;
+
+	/* Set DCO as source for
+	 * MCLK: Masters clock drives CPU and some peripherals
+	 * SMCLK: Sub system clock drives some peripherals
+	 * */
+	//BCSCTL2 default 
+
+}
+
 static void watchdog_stop(void) { WDTCTL = WDTPW + WDTHOLD; }
 
 void mcu_init(void)
 {
     watchdog_stop();
+    init_clocks();
     io_init();
     _enable_interrupts(); // enable interrupts globally
 }
